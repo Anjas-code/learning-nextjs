9618562c948db7a04d2884252cf76dc688b152ad
Anjas <arifanjas.code@gmail.com>
2024-04-29 16:19:55 +0700

    chapter 5

---
+++ app/constants.ts
@@ -0,0 +1,16 @@
+import {
+  DocumentDuplicateIcon,
+  HomeIcon,
+  UserGroupIcon,
+} from '@heroicons/react/24/outline';
+import { SIDEBAR_TYPE } from './type';
+
+export const SIDEBAR_CONSTANT: SIDEBAR_TYPE = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+];
---
+++ app/type.ts
@@ -0,0 +1,7 @@
+export type SIDEBAR_TYPE = {
+  name: string;
+  href: string;
+  icon: React.ForwardRefExoticComponent<
+    Omit<React.SVGProps<SVGSVGElement>, 'ref'>
+  >;
+}[];
